65c561902933d78b6c688f62c08e2c5384ecdaab
Pavel Foujeu <foujeupavel@gmail.com>
2020-05-28 05:02:44 +0100

    Fixed design for the Poster Item on the Main screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import Text from '~/components/common/Text';
+import React, {useState} from 'react';
 import Container from '~/components/common/Container';
+import {View} from 'react-native';
+import StatusBar from '~/components/common/StatusBar';
+import {SearchBar} from 'react-native-elements';
+import {NavigationScreenProp} from 'react-navigation';
+import Poster from '~/screens/Home/components/Poster';
+import {IMovie} from '~/types/Movie';
 
-const Home = () => {
+const Home = ({navigation}: {navigation: NavigationScreenProp<any>}) => {
+  const [search, setSearch] = useState('');
+
+  const updateSearch = (search: string) => {
+    setSearch(search);
+  };
+
+  const sampleMovie: IMovie = {
+    id:"xdtplkjg-oikjhgd",
+    title:"Titre",
+    posterPath:require('~/assets/images/poster.jpeg'),
+    releaseDate:"20-40-2009",
+    description:"Just an amazing movie"
+  };
+
   return (
     <Container>
-      <Text> Home</Text>
+      <StatusBar/>
+      <View style={{marginTop: 20}}>
+        <SearchBar
+          inputContainerStyle={{height: 37}}
+          containerStyle={{borderRadius: 10, borderWidth: 1, marginHorizontal:10}}
+          placeholder="Discover"
+          onChangeText={updateSearch}
+          value={search}/>
+          <Poster movie={sampleMovie}/>
+      </View>
     </Container>
   );
 };
--- src/theming/colors.ts
+++ src/theming/colors.ts
@@ -6,7 +6,10 @@ const colors = {
   pagination: '#7B7B7B',
   warning: '#F9AA33',
   filterBlack: '#405467',
-  blackOverlay: 'rgba(0,0,0,0.9)',
+  black100:'rgba(0,0,0,0.7)',
+  black40: 'rgba(0,0,0,0.1)',
+  black00: 'rgba(0,0,0,0.1)',
+  blackOverlay: 'rgba(0,0,0,0.1)',
   darkgray: 'rgb(151,151,151)',
   lightgray: 'rgba(151,151,151,0.4)',
   red: 'rgb(255,61,61)',
@@ -15,6 +18,8 @@ const colors = {
   green: '#01D475',
   lightGrey: '#9A9A9A',
   darkGrey: '#282828',
+  transparent: 'rgba(0,0,0,0)',
+
 };
 
 export default colors;
