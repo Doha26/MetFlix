29a71fdfdaee72166609044d43bf0b40801f6771
Pavel Foujeu <foujeupavel@gmail.com>
2020-05-29 03:23:42 +0100

    finished detail screen design and added shared element transitions

---
+++ src/screens/Home/components/Documentary/style.ts
@@ -0,0 +1,35 @@
+import {Dimensions, StyleSheet} from "react-native";
+import Colors from "~/theming/colors";
+
+const {width}= Dimensions.get("window");
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: (width / 3) + 5,
+        height: 250, marginTop: 10,
+    },
+
+    imageWrapper: {
+        flex: 1,
+        borderRadius:8,
+        overflow: 'hidden',
+        alignItems: 'center',
+        backgroundColor: 'orange',
+        position: 'relative',
+    },
+    image: {
+        flex: 1,
+        resizeMode:"stretch"
+    },
+    detailsWraper: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        backgroundColor: Colors.lightgray,
+    },
+});
+export default styles;
---
+++ src/screens/Home/components/styles.ts
@@ -0,0 +1,34 @@
+import {StyleSheet} from "react-native";
+import Colors from "~/theming/colors";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 10,
+        height: 250, marginTop: 110,
+    },
+    imageWrapper: {
+        flex: 1,
+        borderRadius:8,
+        overflow: 'hidden',
+        alignItems: 'center',
+        backgroundColor: 'orange',
+        position: 'relative',
+        transform: [{scale: 1}],
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+    },
+    detailsWraper: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        backgroundColor: Colors.lightgray,
+    },
+});
+
+export default styles;
