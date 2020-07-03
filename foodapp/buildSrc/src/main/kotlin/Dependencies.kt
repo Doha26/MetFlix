6767bb6dcbeffcc68a30fbedd942a7b4e4cd4b39
Pavel Foujeu <foujeupavel@gmail.com>
2020-07-04 00:05:48 +0100

    setting up root project with core dependencies

---
+++ foodapp/buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,75 @@
+object Testing {
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.5"
+    const val room = "androidx.room:room-testing:2.2.5"
+    const val jUnit = "junit:junit:4.13"
+    const val extJUnit = "androidx.test.ext:junit:1.1.1"
+    const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
+    const val okHttp = "com.squareup.okhttp3:mockwebserver:4.4.0"
+    const val core = "androidx.arch.core:core-testing:2.1.0"
+}
+
+object Dependencies {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72"
+    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
+    const val gradle = "com.android.tools.build:gradle:4.0.0"
+    const val daggerHilt = "com.google.dagger:hilt-android-gradle-plugin:2.28-alpha"
+    const val ktLint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
+    const val materialDesign = "com.google.android.material:material:1.1.0"
+    const val materialDialog = "com.shreyaspatil:MaterialDialog:2.1"
+    const val coil = "io.coil-kt:coil:0.9.5"
+
+    // Third party
+
+    const val calligraphy = "uk.co.chrisjenx:calligraphy:2.3.0"
+    const val sdp = "com.intuit.sdp:sdp-android:1.0.6"
+    const val glide = "com.github.bumptech.glide:glide:4.10.0"
+    const val multidex = "androidx.multidex:multidex:2.0.1"
+}
+
+object Lifecycle {
+    const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0"
+    const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
+}
+
+object Navigation {
+    const val navigationUI = "androidx.navigation:navigation-ui-ktx:2.3.0"
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:2.3.0"
+    const val navigationTesting = "androidx.navigation:navigation-testing:2.3.0"
+    const val navigationDynamicFeature = "androidx.navigation:navigation-dynamic-features-fragment:2.3.0"
+}
+
+object Hilt {
+    const val daggerCompiler = "com.google.dagger:hilt-android-compiler:2.28-alpha"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:1.0.0-alpha01"
+    const val hiltViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha01"
+    const val hiltAndroid = "com.google.dagger:hilt-android:2.28-alpha"
+}
+
+object Moshi {
+    const val moshi = "com.squareup.moshi:moshi-kotlin:1.9.2"
+    const val codeGen = "com.squareup.moshi:moshi-kotlin-codegen:1.9.2"
+}
+
+object Retrofit {
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.8.1"
+    const val moshiRetrofitConverter = "com.squareup.retrofit2:converter-moshi:2.7.2"
+}
+
+object Coroutines {
+    const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5"
+    const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5"
+}
+
+object Android {
+    const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val activityKtx = "androidx.activity:activity-ktx:1.1.0"
+    const val coreKtx = "androidx.core:core-ktx:1.2.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
+}
+
+object Room {
+    const val compiler = "androidx.room:room-compiler:2.2.5"
+    const val ktx = "androidx.room:room-ktx:2.2.5"
+    const val runtime = "androidx.room:room-runtime:2.2.5"
+}
